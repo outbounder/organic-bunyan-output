b88fcba8ff0be771039467e9db77a8022b50205b
Boris Filipov <obiwonn@gmail.com>
2014-09-01 14:03:01 +0300

    upgrade to bunyan peer dep 1.0.1

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,11 +1,14 @@
 describe("organic-console-bunyan-output", function(){
-  var Plasma = require("organic").Plasma
+  var Plasma = require("organic-plasma")
   var Organel = require("../index")
 
   var plasma = new Plasma()
 
   it("creates and invokes bunyan methods", function(){
-    var instance = new Organel(plasma, {reactOn: "output", bunyan: {name: "TEST"}})
+    var instance = new Organel(plasma, {
+      reactOn: "output", 
+      bunyan: { name: "TEST" }
+    })
     plasma.emit({
       type: "output",
       method: "log",
